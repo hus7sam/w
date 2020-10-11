51efbfeaa6a1bfc443e9337cd6a97ac9e68d7c6f
h.alsaedi <hrb7070@hotmail.com>
2020-10-11 14:59:38 +0300

    u

---
+++ inedx.php
@@ -0,0 +1,87 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>فينا خير</title>
+</head>
+<body>
+
+<div class="bar">
+    <h2>| فينا خير  </h2>
+    <p>خير الناس أنفعهم للناس ..</p>
+
+</div>
+
+<div class="head">
+<h1>  أختر أي فئة تساعد !</h1>
+    <h2>(يَا أَيُّهَا الَّذِينَ آمَنُوا ارْكَعُوا وَاسْجُدُوا وَاعْبُدُوا رَبَّكُمْ وَافْعَلُوا الْخَيْرَ لَعَلَّكُمْ تُفْلِحُونَ) </h2>
+
+    <a href="inedx.php"> الصحة</a>
+    <a href="inedx.php"> التعليم</a>
+    <a href="inedx.php"> الغذاء</a>
+    <a href="inedx.php"> الكسوة</a>
+<!--    <a href="inedx.php"> تبرعات</a>-->
+</div>
+
+<div class="head" style="background-color: #c1bebe;">
+<h1>  أهداف عمل الخير</h1>
+    <ol>
+        <li>تحقيق رضا الله تعالى، والحصول على الأجر والثواب.</li>
+        <li>فعل الخير بما يضمن للآخرين الحياة الكريمة.</li>
+        <li>نشر المعروف بين الناس، وتشجيعهم على التعاون والتواصل والبرّ.</li>
+        <li>نشر القيم الإسلامية بين الناس، كالتضامن، والتسامح، والتعاون.</li>
+        <li>صيانة كرامة الفقراء والمحتاجين بتقديم المساعدة إليهم دون حاجتهم لذلّ السؤال.</li>
+        <li>تربية على معاني العطاء والابتكار.</li>
+        <li>تربية الأجيال على معنى المواطنة الصالحة</li>
+    </ol>
+
+</div>
+
+<div class="head">
+    <h1>   التسجيل</h1>
+
+        <form>
+            <label for="fname">First Name</label>
+            <input type="text" id="fname" name="fname">
+            <label for="lname">Last Name</label>
+            <input type="text" id="lname" name="lname">
+
+            <textarea name="t_text" id="t_text"></textarea>
+        </form>
+
+
+</div>
+
+
+<footer>
+    <ul>
+
+        <li> لتواصل عبر البريد الالكتوني
+            <ul>
+                <li><a href="mailto:hege@example.com">hege@example.com</a></li>
+
+            </ul>
+        </li>
+
+        <li>  لتواصل عبر الواتس
+            <ul>
+                <li><a href="https://wa.me/+996546668820">    0546668820  </a></li>
+
+            </ul>
+        </li>
+
+    </ul>
+
+<h3> جميع الحقوق محفوظة </h3>
+
+
+</footer>
+</body>
+</html>
